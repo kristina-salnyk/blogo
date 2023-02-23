ebd8364a0efb0c0df09f70438b2e7cfccd14a57c
Kristina Salnyk <kristina.salnyk@gmail.com>
2023-02-23 09:37:32 +0100

    bugfix(button): add fix to button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,8 @@
-import { Platform } from "react-native";
 import { Button as ButtonStyled, Text } from "./Button.styled";
 
 const Button = ({ text, onPress }) => {
   return (
-    <ButtonStyled os={Platform.OS} onPress={onPress} style={{ elevation: 3 }}>
+    <ButtonStyled onPress={onPress} style={{ elevation: 3 }}>
       <Text>{text}</Text>
     </ButtonStyled>
   );
--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -1,9 +1,10 @@
 import styled from "styled-components/native";
+import { Platform } from "react-native";
 
 export const Button = styled.TouchableOpacity`
   margin-top: ${({ theme }) => theme.spacing[2]}px;
   margin-bottom: ${({ theme }) => theme.spacing[0]}px;
-  padding: ${({ os, theme }) => (os === "ios" ? theme.spacing[1] : 12)}px 0;
+  padding: ${({ theme }) => (Platform.OS === "ios" ? theme.spacing[1] : 12)}px 0;
   background-color: ${({ theme }) => theme.colors.main};
   border-radius: ${({ theme }) => theme.shape.borderRadius.xs}px;
   align-items: center;
